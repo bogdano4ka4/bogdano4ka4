2cf9530af5c89eefce89daf53d015df3e29dd0fa
bogdano4ka4 <bogdano4ka1@gmail.com>
2018-11-17 22:46:08 +0200

    dont work babel

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+ 
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'js/main.js',
+    },
+};
+
+// ... contents of webpack.config.js
+module.exports = {
+    // ...previous Webpack config...
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            // ...additional rules...
+        ],
+    },
+};
